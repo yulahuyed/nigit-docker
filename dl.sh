a25e88a79ffbc2feda3dc5bafeb1b8bea9fa040e
yulahuyed <yulahuyed@vpsorg.top>
2018-02-11 22:53:23 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -2,7 +2,7 @@
 
 case $DOWN in
   aria2.*baidu.*) nohup $DOWN > 1.log 2>&1 & ;;
-  http.*youtu.*be.*) nohup youtube-dl -f bestvideo+bestaudio/best -o "/dl/temp/%(title)s-%(id)s.%(ext)s" "https://www.youtube.com/watch?v=$DOWN" > 1.log 2>&1 & ;;
+  http.*youtu.*be.*) nohup youtube-dl -f bestvideo+bestaudio/best -o "/dl/temp/%(title)s-%(id)s.%(ext)s" "$DOWN" > 1.log 2>&1 & ;;
   *) nohup wget -P /dl/temp "$DOWN" > 1.log 2>&1 & ;;
 esac
 
