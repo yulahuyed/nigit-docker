3e32d6361d521fbe1bcb4e95715abe07dfca3cf4
yulahuyed <yulahuyed@vpsorg.top>
2018-02-11 21:36:35 +0800

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -f "~/.config/rclone/rclone.conf" ]
+then 
+rm ~/.config/rclone/rclone.conf
+fi
+wget -O ~/.config/rclone/rclone.conf ${RCLONE_CONFIG}
+NAME=$(cat ~/.config/rclone/rclone.conf | grep -oE '\[.*\]' | sed 's/\[\(.*\)\]/\1/g')
+nohup rclone copy /dl/downloads/* $NAME: &
