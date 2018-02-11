f19d9cdf1b8f03a54359eedb133ae8c82dba89f8
yulahuyed <yulahuyed@vpsorg.top>
2018-02-11 22:23:43 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-if [[ $DOWN =~ "aria2.*baidu" ]]
-then
- nohup $DOWN > 1.log 2>&1 &
-else
-  if [[ $DOWN =~ "youtu.*be" ]]
-  then
-  youtube-dl -f bestvideo+bestaudio/best -o "/dl/downloads/%(title)s-%(id)s.%(ext)s" $DOWN
-  else
-  wget $DOWN
-  fi
-fi
+case $DOWN in
+  aria2.*baidu.*) nohup $DOWN > 1.log 2>&1 & ;;
+  http.*youtu.*be.*) youtube-dl -f bestvideo+bestaudio/best -o "/dl/downloads/%(title)s-%(id)s.%(ext)s" "$DOWN" ;;
+  *) wget "$DOWN" ;;
+esac
 
 if [ "${RCLONE_CONFIG}" ]
 then
