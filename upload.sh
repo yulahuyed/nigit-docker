8964cc194c1b774ac92ce7aed5baaa2471dc7c5b
yulahuyed <yulahuyed@vpsorg.top>
2018-02-11 21:50:02 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -7,3 +7,10 @@ fi
 wget -O ~/.config/rclone/rclone.conf ${RCLONE_CONFIG}
 NAME=$(cat ~/.config/rclone/rclone.conf | grep -oE '\[.*\]' | sed 's/\[\(.*\)\]/\1/g')
 nohup rclone copy /dl/downloads/* $NAME: &
+
+if [ "${SLACK}" ]
+then
+curl -X POST -H 'Content-type: application/json' --data '{"text": "Files have been uploaded!", "channel": "#private", "link_names": 1, "username": "Upload-bot", "icon_emoji": ":monkey_face:"}' ${SLACK}
+fi
+
+rm /dl/downloads/*
