26bd769d4a9dedae05c3d75c300833d25fbaf155
yulahuyed <yulahuyed@vpsorg.top>
2018-02-11 22:38:04 +0800

    Update dl.sh

--- dl.sh
+++ dl.sh
@@ -2,9 +2,11 @@
 
 case $DOWN in
   aria2.*baidu.*) nohup $DOWN > 1.log 2>&1 & ;;
-  http.*youtu.*be.*) youtube-dl -f bestvideo+bestaudio/best -o "/dl/downloads/%(title)s-%(id)s.%(ext)s" "$DOWN" ;;
-  *) wget -P /dl/downloads "$DOWN" ;;
+  http.*youtu.*be.*) nohup youtube-dl -f bestvideo+bestaudio/best -o "/dl/temp/%(title)s-%(id)s.%(ext)s" "$DOWN" > 1.log 2>&1 & ;;
+  *) nohup wget -P /dl/temp "$DOWN" > 1.log 2>&1 & ;;
 esac
+
+mv /dl/temp/* /dl/downloads
 
 if [ "${RCLONE_CONFIG}" ]
 then
