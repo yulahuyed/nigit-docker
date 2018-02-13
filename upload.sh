1958787a48da7a8408dec098b3f8e1809b5a6628
yulahuyed <yulahuyed@vpsorg.top>
2018-02-13 14:39:41 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,10 +3,10 @@
 case $NETDISK in
   gdrive)
     RCLONE_TYPE=$(cat ~/.config/rclone/rclone.conf | grep -oE '\[.*\]' | sed 's/\[\(.*\)\]/\1/g')
-    nohup rclone copy /dl/downloads/* $RCLONE_TYPE: &
+    nohup rclone copy -vv /dl/downloads/* $RCLONE_TYPE: > upload.log 2>&1 &
   ;;
   od4b)
-    nohup onedrive -d "dl/downloads/$FILENAME" > od.log 2>&1 &
+    nohup onedrive -d "dl/downloads/$FILENAME" > upload.log 2>&1 &
   ;;
 esac
 
