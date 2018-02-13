d9f8198c415333a6d6d6bf73cc3e7f469f32c7c0
yulahuyed <yulahuyed@vpsorg.top>
2018-02-13 10:42:24 +0800

    Update dl-notice.sh

--- dl-notice.sh
+++ dl-notice.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 while [ true ]
 do
-sleep 5
 DL_LOGS=$(tail -n 10 /dl/1.log)
 case ${DL_LOGS} in
   .*OK.*|.*completed.*|.*save.*|.*100%.*)
+    TYPE=$(cat type.txt) && rm type.txt
+    if [ "$TYPE" == "BDDL" ]
+    then
+    MD5_CHECK=$(md5sum /dl/downloads/$FILENAME)
+      if [ "${MD5_CHECK}" == "${MD5}" ]
+      then
+      curl -X POST -H 'Content-type: application/json' --data '{"text": "Download Completed!\n${FILENAME}", "channel": "#private", "link_names": 1, "username": "Upload-bot", "icon_emoji": ":monkey_face:"}' ${SLACK}
+      else
+      curl -X POST -H 'Content-type: application/json' --data '{"text": "Download Completed, but MD5 is incorrectly!\n${FILENAME}", "channel": "#private", "link_names": 1, "username": "Upload-bot", "icon_emoji": ":monkey_face:"}' ${SLACK}
+      fi
+    fi
     nohup bash /dl/upload.sh &
     exit 0
   ;;
@@ -13,5 +23,6 @@ case ${DL_LOGS} in
     exit 1
   ;;
 esac
+sleep 5
 done
 
