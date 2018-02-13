5a65ef7e16f81969d51eb95f7d9a71c4857bcc65
yulahuyed <yulahuyed@vpsorg.top>
2018-02-13 09:31:40 +0800

    Update dl-notice.sh

--- dl-notice.sh
+++ dl-notice.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-
+while [ true ]
+do
+sleep 5
 DL_LOGS=$(tail -n 10 /dl/1.log)
 case ${DL_LOGS} in
   .*OK.*|.*completed.*|.*save.*|.*100%.*)
     mv /dl/temp/* /dl/downloads/
     bash /dl/upload.sh
+    exit 0
   ;;
   .*error.*|.*ERROR.*) 
     curl -X POST -H 'Content-type: application/json' --data '{"text": "Download ERROR!\n${DL_LOGS}", "channel": "#private", "link_names": 1, "username": "Upload-bot", "icon_emoji": ":monkey_face:"}' ${SLACK}
+    exit 1
   ;;
 esac
+done
 
